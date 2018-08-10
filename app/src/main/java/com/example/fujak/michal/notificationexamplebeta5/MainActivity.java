3e21da73d466913eea0cb5e2fde476b780a633fc
= <michalfujak005@gmail.com>
2018-08-10 11:31:23 +0200

    add Toast

--- app/src/main/java/com/example/fujak/michal/notificationexamplebeta5/MainActivity.java
+++ app/src/main/java/com/example/fujak/michal/notificationexamplebeta5/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.fujak.michal.notificationexamplebeta5;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Context cont;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button_submit = (Button) findViewById(R.id.button_action_notifi);
+        button_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Stlacene tlacitko Submit", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
 }
